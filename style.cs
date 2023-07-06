934106bef2d5434e9d1b2fc549e6492602917fff
Tejindra khatri <36929353+Tejindrakhatri@users.noreply.github.com>
2023-07-06 12:56:33 -0400

    Update style.cs

--- style.cs
+++ style.cs
@@ -1,1 +1,29 @@
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:300,400,700&display=swap');
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 
+html{
+    font-size: 10px;
+    font-family: 'Montserrat', san-serif;
+    scroll-behavior: smooth;
+}
+a{
+    text-decoration: none;
+    color: inherit;
+}
+.container{
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+img {
+	height: 100%;
+	width: 100%;
+	object-fit: cover;
+}
