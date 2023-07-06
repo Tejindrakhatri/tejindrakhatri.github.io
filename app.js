2b2c555888d373bacb3dc294eaa48e4bbe92ffe0
Tejindra khatri <36929353+Tejindrakhatri@users.noreply.github.com>
2023-07-06 18:13:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_items = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
 
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
+
+menu_items.addEventListener('click', (event) => {
+  if (event.target.matches('.header .nav-bar .nav-list ul a')) {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+  }
+});
